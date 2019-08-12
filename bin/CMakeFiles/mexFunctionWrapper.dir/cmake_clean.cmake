57c776c2cdbe587375cf7270285e543dc5556111
Brent <brent.foster@yale.edu>
2019-08-12 12:36:00 -0400

    Auto stash before rebase of "refs/heads/CASImgs"

---
+++ bin/CMakeFiles/mexFunctionWrapper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mexFunctionWrapper.dir/mexFunctionWrapper.cpp.o"
+  "mexFunctionWrapper.mexa64"
+  "mexFunctionWrapper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mexFunctionWrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
