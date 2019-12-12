a58955b5e4a3cb0cb437cb0e2d0a442d0e4035fc
Brent <brent.foster@yale.edu>
2019-12-12 14:36:02 -0500

    Added catch to easyMontage if the figure was deleted before plotting

--- src/Matlab/utils/easyMontage.m
+++ src/Matlab/utils/easyMontage.m
@@ -1,3 +1,7 @@
 function easyMontage(x,figNum)
 figure(figNum);
-montage(reshape(x,[size(x,1) size(x,2) 1 size(x,3)]),'DisplayRange',[]);
+try
+    montage(reshape(x,[size(x,1) size(x,2) 1 size(x,3)]),'DisplayRange',[]);
+catch
+   disp("easyMontage Failed. Likely an Invalid or deleted object or a NaN.") 
+end
