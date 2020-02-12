d20b73c5b3e4cff3e64d8b7f902c33670245c113
Brent <brent.foster@yale.edu>
2020-02-12 13:40:12 -0500

    Improved the error message for easyMontage

--- src/Matlab/utils/easyMontage.m
+++ src/Matlab/utils/easyMontage.m
@@ -3,5 +3,5 @@ figure(figNum);
 try
     montage(reshape(x,[size(x,1) size(x,2) 1 size(x,3)]),'DisplayRange',[]);
 catch
-   disp("easyMontage Failed. Likely an Invalid or deleted object or a NaN.") 
+   disp("easyMontage Failed. Likely the figure was closed or the data contains a NaN value.") 
 end
